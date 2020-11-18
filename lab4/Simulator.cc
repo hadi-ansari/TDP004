4e481ba4967327b624dbafda093b59337e4e5b13
Hadi <hadan326@student.liu.se>
2020-11-18 15:07:07 +0100

    Fortsättning på lab4

--- lab4/Simulator.cc
+++ lab4/Simulator.cc
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <vector>
 #include <string>
 
 #include "Simulator.h"
 
+
+
+/*       Connection          */
 struct Connection
 {
   Connection()
@@ -15,42 +19,95 @@ struct Connection
   double charge;
 };
 
-Component::Component(Connection& p, Connection& n)
-  :p{p}, n{n}
+
+/*      Component          */
+
+Component::Component(std::string name, Connection& p, Connection& n)
+  :name{name}, p{p}, n{n}
 {}
 
-// void Component::set_p(double value)
-// {
-//   p.charge = value;
-// }
-// void Component::set_n(double value)
-// {
-//   n.charge = value;
-// }
-// double Component::get_p()const
-// {
-//   return p.charge;
-// }
-// double Component::get_n()const
-// {
-//   return n.charge;
-// }
+
+/*       Ressistor          */
 Ressistor::Ressistor(std::string name, double resistance, Connection& p, Connection& n)
-  : Component::Component{p, n}, name{name}, resistance{resistance}
+  : Component::Component{name, p, n}, resistance{resistance}
 {}
 
 void Ressistor::simulate(double t)
 {
-  
   n.charge += (p.charge - n.charge) / resistance * t;
-  std::cout << n.charge << std::endl;
+}
+double Ressistor::get_voltage()
+{
+  return p.charge - n.charge;
+}
+double Ressistor::get_current()
+{
+  return get_voltage() / resistance; 
+}
+
+
+/*       Battery          */
+Battery::Battery(std::string name, double voltage, Connection& p, Connection& n)
+  : Component::Component{name, p, n}, voltage{voltage}
+{}
+void Battery::simulate(double t)
+{
+   p.charge = voltage;
+   n.charge = 0;
+}
+double Battery::get_voltage()
+{
+  return voltage;
+}
+double Battery::get_current()
+{
+  return 0;
+}
+void Battery::set_voltage(double const v)
+{
+  voltage = v;
+}
+
+
+/*       Capacitor          */
+Capacitor::Capacitor(std::string name, double capacity,
+		     double load, Connection& p, Connection& n)
+  : Component::Component{name, p, n}, capacity{capacity}, load{load}
+{}
+void Capacitor::simulate(double t)
+{
+  n.charge += capacity * (p.charge - n.charge) * t;
+}
+double Capacitor::get_voltage()
+{
+  return p.charge - n.charge;
+}
+double Capacitor::get_current()
+{
+  return capacity * ( get_voltage() - load);
+}
+
+void simulate(std::vector<Component*> n, int number, int print, double t)
+{
+  for( int i = 0; i < number; ++i)
+    {
+      for(Component* component: n)
+	{
+	  component -> simulate(t);
+	}
+    }
 }
 
 int main()
 {
-  Connection p{24}, n{0};
-  Ressistor r{"R1", 2, p, n};
-  r.simulate(0.1);
-  std::cout << "hej" << std::endl;
+  Connection p, n;
+  std::vector <Component*> net{};
+  Battery B1{"Bat", 21, p, n};
+  net.push_back(new Battery("Bat", 24.0, p, n));
+  net.push_back(new Ressistor{"R1", 6.0, p, n});
+  net.push_back(new Ressistor{"R2", 8.0, p, n});
+  simulate(net, 1000, 10, 0.1);
+  std::cout << "ok" << std::endl;
+  //  std::cout << "P: " << p.charge << "\nN: " << n.charge << std::endl;
   return 0;
 }
--- lab4/Simulator.h
+++ lab4/Simulator.h
@@ -6,14 +6,12 @@ struct Connection;
 class Component
 {
  public:
-  Component(Connection& , Connection& );
+  Component(std::string, Connection& ,Connection& );
   virtual void simulate(double)=0;
-  // void set_p(double );
-  // void set_n(double );
-  // double get_p()const;
-  // double get_n()const;
-  double current;
-  double voltage;
+  virtual double get_voltage()=0;
+  virtual double get_current()=0;
+
+  std::string name;
   Connection& p;
   Connection& n;
 };
@@ -21,19 +19,40 @@ class Component
 class Ressistor: public Component
 {
  public:
-  Ressistor(std::string , double, Connection& , Connection&);
+  Ressistor(std::string , double, Connection&, Connection&);
   void simulate(double ) override;
+  double get_voltage() override;
+  double get_current() override;
+  
  private:
-  std::string name;
   double resistance;
 };
 
-// class Capacitor: public Component
-// {
-//  public:
+class Battery: public Component
+{
+public:
+  Battery(std::string , double, Connection&, Connection&);
+  void simulate(double) override;
+  double get_voltage() override;
+  double get_current() override;
+  void set_voltage(double const);
   
-//  private:
+private:
+  double voltage;
   
-// };
+};
+  
+class Capacitor: public Component
+{
+ public:
+  Capacitor(std::string , double, double, Connection&, Connection&);
+  void simulate(double) override;
+  double get_voltage() override;
+  double get_current() override;
+  
+ private:
+  double capacity;
+  double load;
+};
 
 #endif
