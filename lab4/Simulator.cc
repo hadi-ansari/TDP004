7c42388b08e65cc1750784948a24120db3946a3b
Hadi <hadan326@student.liu.se>
2020-11-18 10:08:46 +0100

    Uppdatering på lab4

--- lab4/Simulator.cc
+++ lab4/Simulator.cc
@@ -1,4 +1,56 @@
 #include <iostream>
+#include <string>
 
 #include "Simulator.h"
 
+struct Connection
+{
+  Connection()
+    : charge{0}
+  {}
+  Connection(double v)
+    : charge{v}
+  {}
+  
+  double charge;
+};
+
+Component::Component(Connection& p, Connection& n)
+  :p{p}, n{n}
+{}
+
+// void Component::set_p(double value)
+// {
+//   p.charge = value;
+// }
+// void Component::set_n(double value)
+// {
+//   n.charge = value;
+// }
+// double Component::get_p()const
+// {
+//   return p.charge;
+// }
+// double Component::get_n()const
+// {
+//   return n.charge;
+// }
+Ressistor::Ressistor(std::string name, double resistance, Connection& p, Connection& n)
+  : Component::Component{p, n}, name{name}, resistance{resistance}
+{}
+
+void Ressistor::simulate(double t)
+{
+  
+  n.charge += (p.charge - n.charge) / resistance * t;
+  std::cout << n.charge << std::endl;
+}
+
+int main()
+{
+  Connection p{24}, n{0};
+  Ressistor r{"R1", 2, p, n};
+  r.simulate(0.1);
+  std::cout << "hej" << std::endl;
+  return 0;
+}
--- lab4/Simulator.h
+++ lab4/Simulator.h
@@ -1,29 +1,39 @@
 #ifndef SIMULATOR_H
 #define SIMULATOR_H
+
+struct Connection;
+
 class Component
 {
  public:
-  virtual simulate()=0;
-
+  Component(Connection& , Connection& );
+  virtual void simulate(double)=0;
+  // void set_p(double );
+  // void set_n(double );
+  // double get_p()const;
+  // double get_n()const;
+  double current;
+  double voltage;
+  Connection& p;
+  Connection& n;
 };
 
 class Ressistor: public Component
 {
  public:
-  
+  Ressistor(std::string , double, Connection& , Connection&);
+  void simulate(double ) override;
  private:
-
+  std::string name;
+  double resistance;
 };
 
-class Capacitor: public Component
-{
- public:
+// class Capacitor: public Component
+// {
+//  public:
   
- private:
+//  private:
   
-};
-
-struct Connection
-{};
+// };
 
 #endif
