3e324f009897fdac36d8a390fbae437163d7a35b
Hadi <hadan326@student.liu.se>
2020-11-25 14:11:46 +0100

    lade till Makefile för lab5

---
+++ lab5/CMakeFiles/sorted_array.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hadi/TDP004_labbar/lab5/sorted_array_test.cc" "/home/hadi/TDP004_labbar/lab5/CMakeFiles/sorted_array.dir/sorted_array_test.cc.o"
+  "/home/hadi/TDP004_labbar/lab5/test_main.cc" "/home/hadi/TDP004_labbar/lab5/CMakeFiles/sorted_array.dir/test_main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab5/CMakeFiles/sorted_array.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sorted_array.dir/sorted_array_test.cc.o"
+  "CMakeFiles/sorted_array.dir/test_main.cc.o"
+  "sorted_array"
+  "sorted_array.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sorted_array.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
